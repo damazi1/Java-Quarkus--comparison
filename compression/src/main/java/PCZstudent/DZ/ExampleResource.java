be44948adfc5792a8d0cebbd76a0daf2c027aed5
damazi <dawid.ziora79@gmail.com>
2026-05-20 13:22:25 +0200

    Utworzenie projektu quarkus

---
+++ compression/src/main/java/PCZstudent/DZ/ExampleResource.java
@@ -0,0 +1,16 @@
+package PCZstudent.DZ;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/api/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+}
